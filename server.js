479262f17fa591a2c82227aeaa5b5733c02ebdbb
Arunkumar-12-glitch <arunkumar227264@gmail.com>
2025-06-25 23:33:14 +0530

    Added POST endpoints for creating posts

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-const getRoutes = require('./routes/get');
+const routes = require('./routes'); // Adjust the path to the actual location of routes.js
 
+// Middleware to parse JSON
 app.use(express.json());
-app.use('/api', getRoutes);
+
+// Prefix all routes with '/api'
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 3000;
 
 
-const PORT =3000;
 app.get('/',(req,res)=>{
     res.send("server is running");
 })
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
